b8c423699b696aa7da98ea41216fdfaee8feeecc
David Wong <dclwong@gmail.com>
2020-06-26 14:33:24 +0100

    chrome headless mode added

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,48 +1,57 @@
 import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.Arrays;
 
 @RunWith(Parameterized.class)
 class FooTest {
-    WebDriver driver;
+    static protected WebDriver driver;
     String site;
 
     public void ParameterizedTest(String site) {this.site = site;}
 
-    @Before
-    public void setup() {
-        System.out.println("before");;
-    }
-
-    @Test
-    void gotoGoogle() {
-        System.out.println("gotoGoogle");
+    @BeforeAll
+     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "c://webdriver//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.co.uk");
-        //test
-        driver.close();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("window-size=0x0");
+        driver = new ChromeDriver(chromeOptions);
+        System.out.println("beforeall - initlised webdriver");;
     }
 
     @Test
-    void gotoBBC() {
+    public void gotoBBC() {
         System.out.println("news.bbc.co.uk");
-        System.setProperty("webdriver.chrome.driver", "c://webdriver//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("http://news.bbc.co.uk");
-        driver.close();
     }
+
+    @Test
+    public void gotoGoogle() {
+        System.out.println("gotoGoogle");
+        driver.get("http://www.google.co.uk");
+    }
+
+
 
     @AfterEach
-    void tearDown() {
-        System.out.println("tearDown");
+    public void tearDown() {
+        System.out.println("AfterEach");
 
+    }
+
+    @AfterAll
+    static public void end(){
+        System.out.println("end");
+        driver.close();
     }
     @Parameterized.Parameters()
         public static Iterable<? extends Object> data() {
