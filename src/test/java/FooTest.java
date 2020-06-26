e76868b278bd6acdac47e523d27d9a7218385806
David Wong <dclwong@gmail.com>
2020-06-26 22:24:13 +0100

    chrome headless mode added added notes added sql

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,4 +1,4 @@
-import org.junit.Before;
+import net.bytebuddy.dynamic.scaffold.MethodRegistry;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.sql.*;
 import java.util.Arrays;
 
 @RunWith(Parameterized.class)
@@ -40,7 +41,42 @@ class FooTest {
         driver.get("http://www.google.co.uk");
     }
 
+    @Test
+    public void databaseTest(){
+        Connection conn = null;
 
+        try {
+            DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+            String dbURL = "jdbc:sqlserver://ELSA;databaseName=DWTable";
+            String user = "sa";
+            String pass = "letmein";
+            conn = DriverManager.getConnection(dbURL, user, pass);
+            if (conn != null) {
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+                String sql = "select * from student";
+
+                Statement statement = conn.createStatement();
+                ResultSet result = statement.executeQuery(sql);
+
+                while  (result.next()){
+                    String firstname = result.getString("Firstname");
+                    String surname = result.getString("Surname");
+
+                    System.out.println("Firstname: " + firstname);
+                    System.out.println("Surname: " + surname);
+                }
+
+                conn.close();
+            }
+
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+    }
 
     @AfterEach
     public void tearDown() {
