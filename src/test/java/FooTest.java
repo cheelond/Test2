0f0dd1ff23991f1e3218e534da13d1d2b694a597
David Wong <dclwong@gmail.com>
2020-06-27 12:29:39 +0100

    chrome headless mode added added notes added sql

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,16 +1,22 @@
-import net.bytebuddy.dynamic.scaffold.MethodRegistry;
+import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(Parameterized.class)
 class FooTest {
@@ -23,26 +29,50 @@ class FooTest {
      public static void setup() {
         System.setProperty("webdriver.chrome.driver", "c://webdriver//chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        chromeOptions.addArguments("enable-automation");
         chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("window-size=0x0");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--disable-browser-side-navigation");
+        chromeOptions.addArguments("--disable-gpu");
+
         driver = new ChromeDriver(chromeOptions);
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         System.out.println("beforeall - initlised webdriver");;
     }
 
-    @Test
+    @Ignore
     public void gotoBBC() {
-        System.out.println("news.bbc.co.uk");
+        System.out.printf("***\n");
+        System.out.println("gotoBBC test called");
+        System.out.printf("***\n");
         driver.get("http://news.bbc.co.uk");
     }
 
     @Test
-    public void gotoGoogle() {
-        System.out.println("gotoGoogle");
+    public void gotoGoogle() throws IOException {
+        System.out.printf("***\n");
+        System.out.println("gotoGoogle test called");
+        System.out.printf("***\n");
         driver.get("http://www.google.co.uk");
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(scrFile, new File("c:\\temp\\screenshot.png"));
+        }catch (Exception e){
+            e.getMessage();
+        }
     }
 
     @Test
     public void databaseTest(){
+        System.out.printf("***\n");
+        System.out.println("databaseTest test called");
+        System.out.printf("***\n");
+
         Connection conn = null;
 
         try {
@@ -57,6 +87,7 @@ class FooTest {
                 System.out.println("Driver version: " + dm.getDriverVersion());
                 System.out.println("Product name: " + dm.getDatabaseProductName());
                 System.out.println("Product version: " + dm.getDatabaseProductVersion());
+
                 String sql = "select * from student";
 
                 Statement statement = conn.createStatement();
